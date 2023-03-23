17270fc86bb7b93647759cd38d700b668f0f4c9f
Timo <31155789+TheRealTimo@users.noreply.github.com>
2023-03-23 13:03:01 +0100

    Small fixes
    Small fixes so the IDE doesn't show any error. This is however no testing

--- Library/Library/Student.cs
+++ Library/Library/Student.cs
@@ -3,36 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using System;
+
 
 namespace Library
 {
     internal class Student : Borrower, Observer
     {
-        private string id;
-        private string name;
-
-        public Student(string id, string name)
-        {
-            this.id = id;
-            this.name = name;
-        }
+        public Student(string id, string name) : base(id, name) { }
 
         public void Update(Book book)
         {
-            Console.WriteLine($"Student {name} has been notified that the book '{book.GetTitle()}' is now available.");
-        }
-
-        public override  string GetBorrowerId()
-        {
-            return id;
-        }
-
-        public override  string GetBorrowerName()
-        {
-            return name;
+            Console.WriteLine($"Student {Name} has been notified that the book '{book.GetTitle()}' is now available.");
         }
     }
 }
+
 
