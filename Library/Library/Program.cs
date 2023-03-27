0aae7433b71e954abcf8a25b3f4a4f74f433b08e
tadas0420 <tadas0420@gmail.com>
2023-03-27 15:50:05 +0200

    program

--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -1,1 +1,55 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NovelFactory factory = new NovelFactory();
+
+            LibraryBook book1 = (LibraryBook)factory.CreateBook("Book", "Fernando", "Fiction", "Big S", new DateTime(1925, 4, 10));
+            LibraryBook book2 = (LibraryBook)factory.CreateBook("Patterns", "Gerjan", "Fiction", "JB", new DateTime(1960, 7, 11));
+            LibraryBook book3 = (LibraryBook)factory.CreateBook("Something", "Ro", "Science Fiction", "Flag", new DateTime(1949, 6, 8));
+
+            Observer staff1 = new LibraryStaff("Roy");
+            Observer staff2 = new LibraryStaff("John");
+            book3.RegisterObserver(staff1);
+            book3.RegisterObserver(staff2);
+
+            BookDecorator extendedBook1 = new BorrowingPeriodExtensionDecorator(book1, 7);
+
+            BookDecorator lateFeeBook2 = new LateFeeDecorator(book2, 5.0);
+
+            Borrower borrower1 = new Student("1234", "Rob");
+            Borrower borrower2 = new Student("5678", "Niels");
+            Borrower borrower = new NullStudent("00000", "No Student");
+
+            if (extendedBook1.IsAvailable())
+            {
+                extendedBook1.BorrowBook();
+                Console.WriteLine(borrower1 + " has borrowed the book: " + extendedBook1.GetTitle());
+            }
+
+            if (lateFeeBook2.IsAvailable())
+            {
+                lateFeeBook2.BorrowBook();
+                Console.WriteLine(borrower2 + " has borrowed the book: " + lateFeeBook2.GetTitle());
+            }
+
+            if (!extendedBook1.IsAvailable())
+            {
+                extendedBook1.ReturnBook();
+                Console.WriteLine(borrower1 + "has returned the book: " + extendedBook1.GetTitle());
+            }
+
+            if (!lateFeeBook2.IsAvailable())
+            {
+                lateFeeBook2.ReturnBook();
+                Console.WriteLine(borrower2 + " has returned the book: " + lateFeeBook2.GetTitle());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
